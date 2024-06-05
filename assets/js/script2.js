52999d130522a4cb2d30955a3b890e2416599867
Evans PARFAIT <fani.prft@gmail.com>
2024-06-05 15:20:18 +0200

    Finish the last modifications of the elements in responsive tablet and mobile format

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -2,5 +2,5 @@ const burgerMenu = document.querySelector('.burger-menu');
 const navLinks = document.querySelector('.menu-nav nav ul');
 
 burgerMenu.addEventListener('click', () => {
-  navLinks.classList.toggle('show'); // Ajoute ou supprime la classe "show" pour afficher ou masquer les liens de navigation
+    navLinks.classList.toggle('show'); // Ajoute ou supprime la classe "show" pour afficher ou masquer les liens de navigation
 });
