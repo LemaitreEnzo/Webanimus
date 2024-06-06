59e137e0c5c74c52edf267a40b4cd89bfce4ac6c
Enzo LEMAITRE <lemaitreenzo05@gmail.com>
2024-06-06 11:51:23 +0200

    Update img

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+/=============== SWIPER JS ===============/
+let swiperCards = new Swiper(".card__content", {
+    loop: true,
+    spaceBetween: 32,
+    grabCursor: true,
+
+    // pagination: {
+    //     el: ".swiper-pagination",
+    //     clickable: true,
+    //     dynamicBullets: true,
+    // },
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        600: {
+            slidesPerView: 2,
+        },
+        968: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+const burgerMenu = document.querySelector('.burger-menu');
+const navLinks = document.querySelector('.menu-nav nav ul');
+
+burgerMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('show'); // Ajoute ou supprime la classe "show" pour afficher ou masquer les liens de navigation
+});
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+
